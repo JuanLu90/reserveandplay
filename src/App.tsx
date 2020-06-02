2f082478c5c796bd3ddd230f0c058df66e0e0ca1
juanlumoreno <juanMoreno@socialicity.co.uk>
2020-06-02 11:25:38 +0200

    starting proyect

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import { Switch, Route, Router, Redirect } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import "./App.scss";
 
-function App() {
+import Main from "./components/Main/Main";
+
+const history = createBrowserHistory();
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface IProps { };
+
+const Main: React.FC = () => {
+  return (
+    <div> MAIN </div>
+  );
+}
+
+export default Main;
